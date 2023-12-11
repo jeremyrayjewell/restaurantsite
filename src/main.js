c42289f3d63ea99ad53e598f6ca8443470b8592e
jeremyrayjewell <jeremyrayjewell@gmail.com>
2023-12-11 09:56:54 -0300

    changes

--- src/main.js
+++ src/main.js
@@ -1,31 +1,8 @@
 import { database, ref, get } from "./Data.js";
 
-const setCookie = (cname, cvalue, exdays) => {
-  const d = new Date();
-  d.setTime(d.getTime() + (exdays*24*60*60*1000));
-  const expires = "expires="+ d.toUTCString();
-  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
-
-const getCookie = (cname) => {
-  const name = cname + "=";
-  const decodedCookie = decodeURIComponent(document.cookie);
-  const ca = decodedCookie.split(';');
-  for(let i = 0; i <ca.length; i++) {
-    let c = ca[i];
-    while (c.charAt(0) == ' ') {
-      c = c.substring(1);
-    }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    }
-  }
-  return "";
-}
-
 const itemsRef = ref(database, "items");
 const shop = document.getElementById("shop");
-let basket = JSON.parse(getCookie("data")) || [];
+let basket = JSON.parse(localStorage.getItem("data")) || [];
 
 const generateShop = (itemsData) => {
   shop.innerHTML = itemsData.map((x) => {
@@ -140,7 +117,7 @@ const calculation = () => {
 
 const saveBasket = () => {
   console.log(basket);
-  setCookie("data", JSON.stringify(basket), 7); // 7 days expiration
+  localStorage.setItem("data", JSON.stringify(basket));
 };
 
 calculation();
